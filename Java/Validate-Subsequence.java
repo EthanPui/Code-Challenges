7fb6a89c0d4b464edc8723a8cdef26b498e13185
Ethan Pui <ethanpui@gmail.com>
2022-11-28 13:27:41 +0800

    Update Validate-Subsequence.java

--- Java/Validate-Subsequence.java
+++ Java/Validate-Subsequence.java
@@ -1,15 +1,24 @@
 import java.util.*;
 
 class Program {
-	public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
-		for (int i = 0; i < array.size(); i++) {
+  public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+    int counter =0;
+    List<Integer> checkerArr = new ArrayList<Integer>();
+    for(int i =0; i< sequence.size(); i++){
+      
+      for(int k =0; k < array.size(); k++){
+        if(sequence.get(i) == array.get(k)){
+          checkerArr.add(array.get(k));
+          
+        }
 
-			for (int k = 0; k < sequence.size(); k++) {
-				if (array.get(i) == sequence.get(k)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+      }
+    }
+    // System.out.println(checkerArr);
+    // System.out.println(sequence);
+    if(checkerArr.size() == sequence.size()){
+      return true;
+    }
+    return false;
+  }
 }
